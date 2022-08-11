1f0ddb82cff8919a9e2fcf074aac228e185213f7
kemot84 <you@example.com>
2022-08-11 12:49:55 +0200

    add proj files

---
+++ FizzBuzz.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzGame
+{
+    class FizzBuzz
+    {
+        public string GetData(int number)
+        {
+            if (number % 15 ==0)
+                return "\nFizzBuzz! Gratulacje!";
+
+            if (number % 3 == 0)
+                return "\nFizz! Spróbuj ponownie. Twoja liczba to:";
+            
+            if (number % 5 ==0)
+                return "\nBuzz! Spróbuj ponownie. Twoja liczba to:";
+            
+            return number.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Witaj w grze FizzBuzz! Wpisz liczbę i zobacz swój wynik. Gotowa/y? Wciśnij ENTER:");
+            Console.ReadLine();
+            
+            var fizzBuzz = new FizzBuzz();
+        
+            while (true)
+            {
+                Console.WriteLine("Twoja liczba to: (jeśli chcesz wyjśc z aplikacji wciśnij klawisz E)");
+                var number = GetNumber();
+                Console.WriteLine(fizzBuzz.GetData(number));
+                Console.WriteLine();
+            }
+        }
+        private static int GetNumber()
+        {
+            while (true)
+            {
+                //var val = Console.ReadLine();
+                //if (val.ToUpper() == "T")
+                //    Environment.Exit(0);
+
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("\nPodałeś nieprawidłową wartość. Spróbuj ponownie i wpisz liczbę (lub jeśli chcesz wyjśc z aplikacji wciśnij klawisz E)");
+                    continue;
+                }
+                return number;
+            }
+        }
+    }
+}
